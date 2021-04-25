b5d1683875cfec41b215a6df2ebe008797495638
Mihir Kumar <mihirpandey.13@gmail.com>
2021-04-25 22:31:52 +0530

    Life Cycle Hooks demo This commit shows all the basic (not debugging ones) hooks in composition API.

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
   <Modularity1 />
   <hr />
   <Modularity2 />
+  <hr />
+  <keep-alive><LifecycleHooks /></keep-alive>
 </template>
 
 <script>
@@ -16,8 +18,16 @@ import Reactive from "./topics/reactive";
 import ToRefs from "./topics/to-refs";
 import Modularity1 from "./topics/modularity-1";
 import Modularity2 from "./topics/modularity-2";
+import LifecycleHooks from "./topics/lifecycle-hooks";
 export default {
   name: "App",
-  components: { RefsAndComputed, Reactive, ToRefs, Modularity1, Modularity2 },
+  components: {
+    RefsAndComputed,
+    Reactive,
+    ToRefs,
+    Modularity1,
+    Modularity2,
+    LifecycleHooks,
+  },
 };
 </script>
---
+++ src/topics/lifecycle-hooks.vue
@@ -0,0 +1,49 @@
+<template>
+  <p>Space left: {{ eventObj.roomLeft }} out of {{ eventObj.capacity }}</p>
+  <ul>
+    <li v-for="(name, index) of eventObj.attending" :key="index">
+      {{ name }}
+    </li>
+  </ul>
+  <button @click="incrementCapacity">Increase Capacity</button>
+</template>
+
+<script>
+import useEventSpace from "@/use/event-space";
+import { onActivated, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onDeactivated, onErrorCaptured, onMounted, onUnmounted, onUpdated } from 'vue';
+export default {
+  name: "LifecycleHooks",
+  setup() {
+    onBeforeMount(() => {
+      console.log('on Before Mount');
+    })
+    onMounted(() => {
+      console.log('on Mounted');
+    })
+    onBeforeUpdate(() => {
+      console.log('on Before Update');
+    })
+    onUpdated(() => {
+      console.log('on Updated');
+    })
+    onBeforeUnmount(() => {
+      console.log('on Before Unmount');
+    })
+    onUnmounted(() => {
+      console.log('on Unmounted');
+    })
+    onActivated(() => { // for keep alive components
+      console.log('on Activated');
+    });
+    onDeactivated(() => { // for keep alive components
+      console.log('on Deactivated');
+    });
+    onErrorCaptured((err) => { // for keep alive components
+      console.log('on Error Captured', err);
+    });
+    return {
+      ...useEventSpace(), // how about destructuring to use roomLeft directly?, see modularity-2
+    };
+  },
+};
+</script>
